55899ae80115a56bcad72264f51948435b0414c6
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-11-23 08:53:47 -0300

    Commit inicial. Cria projetos e define docker-compose

---
+++ consumer-api/src/test/java/br/com/comicskafka/consumerapi/ConsumerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.comicskafka.consumerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ producer-api/src/test/java/br/com/comicskafka/producerapi/ProducerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.comicskafka.producerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
